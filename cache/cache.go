c47111554cd275741b7b9b9413b56cff19b0eb70
maryonella02 <maryonella02@gmail.com>
2023-03-10 20:56:05 +0200

    Modify cache

--- cache/cache.go
+++ cache/cache.go
@@ -8,64 +8,78 @@ import (
 
 type Cache struct {
 	mu   sync.Mutex
-	data map[string]*entry
-	// list is a doubly-linked list of entries, used for eviction
+	data map[string]*struct {
+		key        string
+		value      interface{}
+		expiration time.Time
+		element    *list.Element
+	}
 	list       *list.List
 	expiration time.Duration
 }
 
-type entry struct {
-	key        string
-	value      interface{}
-	expiration time.Time
-	element    *list.Element
-}
-
-// NewLRUCache creates a new LRU cache with the specified expiration time.
 func NewLRUCache(expiration time.Duration) *Cache {
 	return &Cache{
-		data:       make(map[string]*entry),
+		data: make(map[string]*struct {
+			key        string
+			value      interface{}
+			expiration time.Time
+			element    *list.Element
+		}),
 		list:       list.New(),
 		expiration: expiration,
 	}
 }
 
-// Get returns the value for the given key, if it exists and has not expired.
 func (c *Cache) Get(key string) (interface{}, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if e, ok := c.data[key]; ok {
 		if time.Now().Before(e.expiration) {
-			c.moveToFront(e)
+			c.list.MoveToFront(e.element)
 			return e.value, true
 		}
-		c.delete(e)
+		c.list.Remove(e.element)
+		delete(c.data, key)
 	}
 	return nil, false
 }
 
-// Set sets the value for the given key.
 func (c *Cache) Set(key string, value interface{}) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if e, ok := c.data[key]; ok {
-		c.delete(e)
+		c.list.MoveToFront(e.element)
+		e.value = value
+		e.expiration = time.Now().Add(c.expiration)
+	} else {
+		e := &struct {
+			key        string
+			value      interface{}
+			expiration time.Time
+			element    *list.Element
+		}{
+			key:        key,
+			value:      value,
+			expiration: time.Now().Add(c.expiration),
+			element:    c.list.PushFront(nil),
+		}
+		e.element.Value = e
+		c.data[key] = e
+		if c.list.Len() > 0 && c.expiration > 0 && c.list.Len() > len(c.data) {
+			c.deleteOldest()
+		}
 	}
-	e := &entry{
-		key:        key,
-		value:      value,
-		expiration: time.Now().Add(c.expiration),
-		element:    c.list.PushFront(&entry{}),
-	}
-	*e.element.Value.(*entry) = *e
-	c.data[key] = e
 }
 
-func (c *Cache) delete(e *entry) {
-	delete(c.data, e.key)
-	c.list.Remove(e.element)
-}
-
-func (c *Cache) moveToFront(e *entry) {
-	c.list.MoveToFront(e.element)
+func (c *Cache) deleteOldest() {
+	if ele := c.list.Back(); ele != nil {
+		c.list.Remove(ele)
+		delete(c.data, ele.Value.(*struct {
+			key        string
+			value      interface{}
+			expiration time.Time
+			element    *list.Element
+		}).key)
+	}
 }
