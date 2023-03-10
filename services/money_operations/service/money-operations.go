8525a96a2a14011deead5a3506b8f5ddb70ecddb
maryonella02 <maryonella02@gmail.com>
2023-03-10 21:29:07 +0200

    Add basic database operation in MySql

--- services/money_operations/service/money-operations.go
+++ services/money_operations/service/money-operations.go
@@ -1,16 +1,19 @@
 package service
 
 import (
+	"database/sql"
 	"fmt"
 	"net"
 	"net/rpc"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type MoneyOperations struct {
-	Balance int
-	Port    string
-	Status  string
-	Stats   map[string]int
+	DB     *sql.DB
+	Port   string
+	Status string
+	Stats  map[string]int
 }
 
 type WithdrawRequest struct {
@@ -43,24 +46,73 @@ func (m *MoneyOperations) ServiceStatus(req *ServiceStatusRequest, res *ServiceS
 }
 
 func (m *MoneyOperations) Withdraw(req *WithdrawRequest, res *WithdrawResponse) error {
-	if m.Balance < req.Amount {
+	// start a transaction
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	// check if the balance is sufficient
+	var balance int
+	err = tx.QueryRow("SELECT balance FROM account").Scan(&balance)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	if balance < req.Amount {
+		tx.Rollback()
 		return fmt.Errorf("insufficient funds")
 	}
-	m.Balance -= req.Amount
-	res.Balance = m.Balance
 
+	// update the balance
+	_, err = tx.Exec("UPDATE account SET balance = balance - ?", req.Amount)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// commit the transaction
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	// update stats
 	m.Stats["TotalWithdrawals"]++
+
+	// return the new balance
+	res.Balance = balance - req.Amount
 	return nil
 }
 
 func (m *MoneyOperations) GetBalance(req *GetBalanceRequest, res *GetBalanceResponse) error {
-	res.Balance = m.Balance
+	// retrieve the balance from the database
+	err := m.DB.QueryRow("SELECT balance FROM account").Scan(&res.Balance)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (m *MoneyOperations) Start() error {
+	// initialize the database connection
+	db, err := sql.Open("mysql", "user:password@tcp(host:port)/database")
+	if err != nil {
+		return err
+	}
+	m.DB = db
+
+	// create tables if they don't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS account (balance INT)")
+	if err != nil {
+		return err
+	}
+
+	// register the RPC methods
 	rpc.Register(m)
 	rpc.HandleHTTP()
+
+	// start the RPC server
 	m.Port = "8080"
 	m.Status = "Running"
 	m.Stats = make(map[string]int)
@@ -70,5 +122,6 @@ func (m *MoneyOperations) Start() error {
 		return err
 	}
 	rpc.Accept(ln)
+
 	return nil
 }
