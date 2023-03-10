dbb85396d857949cfa20471ccc1566b3ac9fbb42
maryonella02 <maryonella02@gmail.com>
2023-03-10 21:00:33 +0200

    Add lost changes

--- services/discovery/service/discovery.go
+++ services/discovery/service/discovery.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net"
 	"net/rpc"
 )
@@ -14,6 +17,7 @@ type Service struct {
 // Discovery is a service that allows other services to register themselves and be discovered by clients.
 type Discovery struct {
 	Services []*Service
+	ln       net.Listener
 }
 
 type RegisterRequest struct {
@@ -28,15 +32,31 @@ type ListResponse struct {
 	Services []*Service
 }
 
-func (d *Discovery) Register(req *RegisterRequest, res *RegisterResponse) error {
-	d.Services = append(d.Services, req.Service)
-	return nil
+func (d *Discovery) Register(ctx context.Context, req *RegisterRequest, res *RegisterResponse) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		if err := ValidateServiceData(req.Service); err != nil {
+			return err
+		}
+		if !serviceExists(req.Service, d.Services) {
+			d.Services = append(d.Services, req.Service)
+			return nil
+		}
+		return fmt.Errorf("service already registered")
+	}
 }
 
 // List lists the services registered with the discovery service.
-func (d *Discovery) List(req *ListRequest, res *ListResponse) error {
-	res.Services = d.Services
-	return nil
+func (d *Discovery) List(ctx context.Context, req *ListRequest, res *ListResponse) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		res.Services = d.Services
+		return nil
+	}
 }
 
 func (d *Discovery) Start() error {
@@ -45,7 +65,42 @@ func (d *Discovery) Start() error {
 	if err != nil {
 		return err
 	}
-	rpc.Accept(ln)
+	d.ln = ln
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			go rpc.ServeConn(conn)
+		}
+	}()
 	return nil
+}
+func (d *Discovery) Stop() error {
+	if d.ln != nil {
+		err := d.ln.Close()
+		return err
+	}
+	return nil
+}
 
+func serviceExists(service *Service, services []*Service) bool {
+	for _, s := range services {
+		if s == service {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidateServiceData(service *Service) error {
+	if service.Name == "" {
+		return fmt.Errorf("invalid service name")
+	}
+	if service.Addr == "" {
+		return fmt.Errorf("invalid service address")
+	}
+	return nil
 }
